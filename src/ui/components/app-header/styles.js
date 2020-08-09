20779b190739ab43283022eb151f00a1aaca5e30
emilioheinz <emiliosheinz@gmail.com>
2020-08-09 14:46:54 -0300

    using colors

--- src/ui/components/app-header/styles.js
+++ src/ui/components/app-header/styles.js
@@ -1,10 +1,12 @@
 import { StyleSheet } from 'react-native'
 
+import { COLORS } from '~/res'
+
 export default StyleSheet.create({
   container: {
     height: 90,
     padding: 20,
     justifyContent: 'flex-end',
-    backgroundColor: 'white',
+    backgroundColor: COLORS.WHITE,
   },
 })
