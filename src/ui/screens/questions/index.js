289938d1629c0028c29cff84e0e9a6932484e131
Rhian Lopes da Costa <rhian.costa@cwi.com.br>
2020-08-12 22:45:03 -0300

    feat: introduce initial questions screen

--- src/ui/screens/questions/index.js
+++ src/ui/screens/questions/index.js
@@ -1,30 +1,80 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { View, Dimensions, Image, ScrollView } from 'react-native'
+import Carousel from 'react-native-snap-carousel'
+import { useDispatch } from 'react-redux'
 
+import { COLORS } from '~/res'
 import { NavigationService } from '~/services'
-import { Button, BBText } from '~/ui/components'
+import * as authActions from '~/store/modules/auth/actions'
+import { Button, BBText, Link, CarouselSteps } from '~/ui/components'
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-})
+import styles from './styles'
+
+const images = [
+  'http://lorempixel.com/g/300/310/food',
+  'http://lorempixel.com/g/300/310/sports',
+  'http://lorempixel.com/g/300/310/',
+]
+
+const { width } = Dimensions.get('window')
 
 export function QuestionsScreen() {
+  const dispatch = useDispatch()
+  const carouselRef = useRef()
+
+  const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+
+  function handleSnapToItemEvent(i) {
+    setCurrentSlideIndex(i)
+  }
+
+  function concludeForm() {
+    NavigationService.pushReplacement({ screen: 'DrawerNavigation' })
+    dispatch(authActions.setHasReadTutorial(true))
+  }
+
+  function next() {
+    carouselRef.current.snapToNext()
+  }
+
+  function renderCarouselSteps() {
+    return (
+      <CarouselSteps
+        numberOfSteps={images.length}
+        selectedStep={currentSlideIndex}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <BBText>QUESTIONS SCREEN</BBText>
-      <Button
-        onPress={() => {
-          NavigationService.pushReplacement({ screen: 'DrawerNavigation' })
-        }}
-      >
-        IR PARA O APP
-      </Button>
+      {renderCarouselSteps()}
+      <Carousel
+        data={images}
+        inactiveSlideScale={1}
+        itemWidth={width}
+        ref={carouselRef}
+        renderItem={({ item }) => (
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            style={styles.scrollView}
+          >
+            <BBText size={23} style={styles.title} type='secondary-bold'>
+              Nos conte mais
+            </BBText>
+            <BBText size={17} style={styles.subtitle}>
+              O que você costuma consumir? Quais são suas compras normalmente?
+              Selecione mais de um item.
+            </BBText>
+            <Image source={{ uri: item }} style={styles.image} />
+          </ScrollView>
+        )}
+        sliderWidth={width}
+        onSnapToItem={(i) => handleSnapToItemEvent(i)}
+      />
+      <View style={styles.buttonContainer}>
+        <Button onPress={next}>Avançar</Button>
+      </View>
     </View>
   )
 }
---
+++ src/ui/screens/questions/styles.js
@@ -0,0 +1,35 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 30,
+  },
+  buttonContainer: {
+    marginHorizontal: 40,
+    marginBottom: 30,
+  },
+  image: {
+    width: '100%',
+    height: 310,
+    borderRadius: 15,
+  },
+  subtitle: {
+    marginBottom: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  questionsTitle: {
+    textAlign: 'auto',
+  },
+  questionsSubtitle: {
+    textAlign: 'center',
+  },
+  scrollView: {
+    marginTop: 20,
+    marginHorizontal: 40,
+  },
+})
