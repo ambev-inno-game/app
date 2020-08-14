d22b294e2ea6e5a6ebc594aaba1136f86081f9c1
Rhian Lopes da Costa <rhian.costa@cwi.com.br>
2020-08-13 22:00:14 -0300

    feat: introduce initial questions screen

--- src/ui/screens/questions/index.js
+++ src/ui/screens/questions/index.js
@@ -6,7 +6,7 @@ import { useDispatch } from 'react-redux'
 import { COLORS } from '~/res'
 import { NavigationService } from '~/services'
 import * as authActions from '~/store/modules/auth/actions'
-import { Button, BBText, Link, CarouselSteps } from '~/ui/components'
+import { Button, BBText, CarouselSteps } from '~/ui/components'
 
 import styles from './styles'
 
@@ -16,12 +16,145 @@ const images = [
   'http://lorempixel.com/g/300/310/',
 ]
 
-const { width } = Dimensions.get('window')
+const data = [
+  {
+    id: 1,
+    title: 'Nos conte mais',
+    question:
+      'O que você costuma consumir? Quais são suas compras normalmente? Selecione mais de um item.',
+    options: [
+      {
+        id: 1,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 2,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 3,
+        image: 'http://lorempixel.com/g/100/120/',
+      },
+      {
+        id: 4,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 5,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 6,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+      {
+        id: 7,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 8,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 9,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: 'Quase lá...',
+    question:
+      'O que você gosta de fazer? Quais são seus hábitos de passatempo ou lazer? Selecione mais de um item.',
+    options: [
+      {
+        id: 1,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 2,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 3,
+        image: 'http://lorempixel.com/g/100/120/',
+      },
+      {
+        id: 4,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 5,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 6,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+      {
+        id: 7,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 8,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 9,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: 'Agora é a última',
+    question: 'Bebida para quantos',
+    options: [
+      {
+        id: 1,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 2,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 3,
+        image: 'http://lorempixel.com/g/100/120/',
+      },
+      {
+        id: 4,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 5,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 6,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+      {
+        id: 7,
+        image: 'http://lorempixel.com/g/100/120/food',
+      },
+      {
+        id: 8,
+        image: 'http://lorempixel.com/g/100/120/sports',
+      },
+      {
+        id: 9,
+        image: 'http://lorempixel.com/g/100/120',
+      },
+    ],
+  },
+]
+
+const { width, height } = Dimensions.get('window')
 
 export function QuestionsScreen() {
   const dispatch = useDispatch()
   const carouselRef = useRef()
-
   const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
 
   function handleSnapToItemEvent(i) {
@@ -30,7 +163,6 @@ export function QuestionsScreen() {
 
   function concludeForm() {
     NavigationService.pushReplacement({ screen: 'DrawerNavigation' })
-    dispatch(authActions.setHasReadTutorial(true))
   }
 
   function next() {
@@ -40,9 +172,30 @@ export function QuestionsScreen() {
   function renderCarouselSteps() {
     return (
       <CarouselSteps
-        numberOfSteps={images.length}
+        numberOfSteps={data.length}
         selectedStep={currentSlideIndex}
       />
+    )
+  }
+
+  function renderCards(value) {
+    return value.options.map((item) => {
+      return <Image source={{ uri: item.image }} style={styles.image} />
+    })
+  }
+
+  function renderButton() {
+    if (currentSlideIndex + 1 === data.length) {
+      return (
+        <View style={styles.buttonContainer}>
+          <Button onPress={concludeForm}>Concluir</Button>
+        </View>
+      )
+    }
+    return (
+      <View style={styles.buttonContainer}>
+        <Button onPress={next}>Avançar</Button>
+      </View>
     )
   }
 
@@ -50,31 +203,28 @@ export function QuestionsScreen() {
     <View style={styles.container}>
       {renderCarouselSteps()}
       <Carousel
-        data={images}
+        data={data}
         inactiveSlideScale={1}
+        itemHeight={height - 200}
         itemWidth={width}
         ref={carouselRef}
         renderItem={({ item }) => (
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            style={styles.scrollView}
-          >
-            <BBText size={23} style={styles.title} type='secondary-bold'>
-              Nos conte mais
-            </BBText>
-            <BBText size={17} style={styles.subtitle}>
-              O que você costuma consumir? Quais são suas compras normalmente?
-              Selecione mais de um item.
-            </BBText>
-            <Image source={{ uri: item }} style={styles.image} />
-          </ScrollView>
+          <View style={styles.cardContainer}>
+            <View style={styles.textContainer}>
+              <BBText size={23} style={styles.title} type='secondary-bold'>
+                {item.title}
+              </BBText>
+              <BBText size={17} style={styles.subtitle}>
+                {item.question}
+              </BBText>
+            </View>
+            <View style={styles.imageContainer}>{renderCards(item)}</View>
+          </View>
         )}
         sliderWidth={width}
         onSnapToItem={(i) => handleSnapToItemEvent(i)}
       />
-      <View style={styles.buttonContainer}>
-        <Button onPress={next}>Avançar</Button>
-      </View>
+      {renderButton()}
     </View>
   )
 }
--- src/ui/screens/questions/styles.js
+++ src/ui/screens/questions/styles.js
@@ -10,9 +10,21 @@ export default StyleSheet.create({
     marginHorizontal: 40,
     marginBottom: 30,
   },
+  cardContainer: {
+    marginBottom: 140,
+    alignItems: 'center',
+  },
+  imageContainer: {
+    flexWrap: 'wrap',
+  },
+  textContainer: {
+    paddingLeft: 30,
+    paddingRight: 30,
+  },
   image: {
-    width: '100%',
-    height: 310,
+    width: 100,
+    height: 120,
+    margin: 5,
     borderRadius: 15,
   },
   subtitle: {
