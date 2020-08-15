1dbff79d8826f74c807f1b2e75ad03b93f493ff4
emilioheinz <emiliosheinz@gmail.com>
2020-08-15 18:24:59 -0300

    qr code logic

--- src/ui/screens/qrcode/index.js
+++ src/ui/screens/qrcode/index.js
@@ -1,25 +1,177 @@
-import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import React, { useState, useEffect, useLayoutEffect, useCallback } from 'react'
+import { StyleSheet, View, Alert } from 'react-native'
 
-import { StatusBar } from 'expo-status-bar'
+import { BarCodeScanner } from 'expo-barcode-scanner'
+import * as Permissions from 'expo-permissions'
 
-import { BBText } from '~/ui/components'
+import { useFocusEffect } from '@react-navigation/native'
 
-export function QrCodeScreen() {
-  return (
-    <View style={styles.container}>
-      <BBText size={20} type='secondary-bold'>
-        Você pode scanear suas embalagens entregues nos pontos de coleta aqui!
-      </BBText>
-    </View>
+import { COLORS } from '~/res'
+import { NavigationService } from '~/services'
+import { BBText, ScreenLoader, Button, AppHeader } from '~/ui/components'
+
+import styles from './styles'
+
+const checkInQrCodeId = 'sempre-recicla-official-checkin-qrcode'
+const bottleQrCodeId = 'sempre-recicla-official-bottle-qrcode'
+
+export function QrCodeScreen({ navigation }) {
+  const [hasCameraPermission, setHasCameraPermission] = useState(null)
+  const [hasCheckedIn, setHasCheckedIn] = useState(false)
+  const [readBottles, setReadBottles] = useState(0)
+  const [hasScanned, setHasScanned] = useState(false)
+
+  useFocusEffect(
+    useCallback(() => {
+      setReadBottles(0)
+      setHasScanned(false)
+    }, [])
   )
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    padding: 30,
-  },
-})
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      header: () => <AppHeader title='Leitor' />,
+    })
+  }, [navigation])
+
+  async function getPermissionsAsync() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA)
+    setHasCameraPermission(status === 'granted')
+  }
+
+  useEffect(() => {
+    getPermissionsAsync()
+  }, [])
+
+  function handleBarCodeScanned({ data = '' }) {
+    setHasScanned(true)
+    const isCheckInQrCode = data.match(checkInQrCodeId)
+    const isBottleQrCode = data.match(bottleQrCodeId)
+    const isValidQrCode = isCheckInQrCode || isBottleQrCode
+
+    if (!isValidQrCode) {
+      Alert.alert('Erro', 'Opss. Parece que o QR Code não é válido :(', [
+        {
+          text: 'OK',
+          onPress: () => setHasScanned(false),
+        },
+      ])
+
+      return
+    }
+
+    if (isValidQrCode) {
+      if (isCheckInQrCode) {
+        if (!hasCheckedIn) {
+          setHasCheckedIn(true)
+          Alert.alert(
+            'Checkin feito com sucesso',
+            'Agora você ja pode ler o QR code dos recipientes.',
+            [
+              {
+                text: 'Continuar',
+                onPress: () => setHasScanned(false),
+              },
+            ]
+          )
+          return
+        }
+
+        if (readBottles === 0) {
+          setHasCheckedIn(false)
+          Alert.alert(
+            'Devolução de recipientes cancelada.',
+            'Você precisa escanear algum recipiente antes de fazer o checkout.',
+            [
+              {
+                text: 'Entendi',
+                onPress: () => setHasScanned(false),
+              },
+            ]
+          )
+          return
+        }
+
+        setHasCheckedIn(false)
+        NavigationService.navigate({
+          screen: 'QrCodeReagingSuccessScreen',
+          params: { readBottles },
+        })
+      }
+
+      if (isBottleQrCode) {
+        if (!hasCheckedIn) {
+          Alert.alert(
+            'Erro',
+            'Você precisa fazer o checkin no ponto de coleta antes.',
+            [
+              {
+                text: 'Entendi',
+                onPress: () => setHasScanned(false),
+              },
+            ]
+          )
+          return
+        }
+
+        setReadBottles(readBottles + 1)
+        Alert.alert(
+          'Recipiente escaneado.',
+          'Continue escaneando os recipientes ou finalize o processo.',
+          [
+            {
+              text: 'Ok',
+              onPress: () => {
+                setHasScanned(false)
+              },
+            },
+          ]
+        )
+      }
+    }
+  }
+
+  function renderContent() {
+    if (hasCameraPermission === null) {
+      return <ScreenLoader />
+    }
+
+    if (hasCameraPermission === false) {
+      return (
+        <View style={styles.dennyContainer}>
+          <BBText
+            size={22}
+            style={{ textAlign: 'center' }}
+            type='secondary-bold'
+          >
+            Acesso a camera negado. Precisamos de acesso a sua camera para ler o
+            QR Code!
+          </BBText>
+          <Button onPress={getPermissionsAsync}>Entendido</Button>
+        </View>
+      )
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={{ padding: 15 }}>
+          <BBText
+            color={COLORS.CARIBBEAN_GREEN}
+            size={20}
+            type='secondary-bold'
+          >{`Recipientes escaneado: ${readBottles}`}</BBText>
+        </View>
+        <BarCodeScanner
+          style={StyleSheet.absoluteFillObject}
+          onBarCodeScanned={(params) => {
+            if (!hasScanned) {
+              handleBarCodeScanned(params)
+            }
+          }}
+        />
+      </View>
+    )
+  }
+
+  return renderContent()
+}
---
+++ src/ui/screens/qrcode/styles.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native'
+
+import { COLORS } from '~/res'
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.WHITE,
+  },
+  dennyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 30,
+    backgroundColor: COLORS.WHITE,
+  },
+})
